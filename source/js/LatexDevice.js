c8f7bfd577a1254e880a18e6431fe97cefc4585e
22cun2 <22cun2@naver.com>
2023-01-17 21:11:32 +0900

    버튼 생성

--- source/js/LatexDevice.js
+++ source/js/LatexDevice.js
@@ -1,16 +1,47 @@
 function init(el) {
     const props = { el : el }
-    const latexDevice = new LatexDevice(props);
-    console.log(latexDevice);
-    latexDevice.render();
+    const device = new Device(props);
+    device.render();
 }
-function LatexDevice(props) {
+function Device(props) {
     const { el } = props;
     this.el = el;
+    this.device = document.createElement('div');
+    this.el.appendChild(this.device);
 }
 
-LatexDevice.prototype.render = () => {
-    const device = document.createElement('div');
-    console.log(this.el);
-    this.el.appendChild(device);
+Device.prototype.render = function() {
+    const displayDiv = document.createElement('div');
+    this.device.appendChild(displayDiv);
+    this.display = new Display({ el : displayDiv });
+    for(let i = 0; i < 10; i++) {
+        const buttonArea = document.createElement('div');
+        this.device.appendChild(buttonArea);
+        new Button({el : buttonArea, value : i });
+    }
+}
+
+function Display(props) {
+    const { el } = props;
+    this.el = document.createElement('input');
+    this.el.type = 'text';
+    el.appendChild(this.el);
+}
+
+Display.prototype.setText =  function(text) {
+    this.el.value = text;
+}
+
+Display.prototype.getText = function() {
+    return this.el.value;
+}
+
+function Button(props) {
+    const { el, value, clickEvent } = props;
+    this.el = document.createElement('div');
+    el.appendChild(this.el);
+    this.el.innerText = value;
+    this.el.addEventListener('click',(ev) => {
+        alert(value);
+    });
 }
